ddb33709550b9ac2dad790f716e67bab7c644138
Sam <samuel.scott.jennings@gmail.com>
2024-09-16 10:12:03 +0200

    always exclude tree specific fields from admin

--- polymorphic_treebeard/admin.py
+++ polymorphic_treebeard/admin.py
@@ -4,7 +4,7 @@ from treebeard.admin import TreeAdmin
 from .forms import movepolynodeform_factory
 
 
-class PolymorphicTreeAdmin(TreeAdmin, PolymorphicParentModelAdmin):
+class PolymorphicTreeAdmin(PolymorphicParentModelAdmin, TreeAdmin):
     pass
 
 
@@ -15,3 +15,8 @@ class PolymorphicTreeChildAdmin(TreeAdmin, PolymorphicChildModelAdmin):
 
     def delete_view(self, request, object_id, context=None):
         return super().delete_view(request, object_id, context)
+
+    def get_subclass_fields(self, request, obj=None):
+        subclass_fields = super().get_subclass_fields(request, obj)
+        always_exclude = ["path", "depth", "numchild"]
+        return [f for f in subclass_fields if f not in always_exclude]
